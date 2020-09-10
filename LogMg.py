25455f5d10fe6b23f66715dbd29c280d3fc867c5
sajadi <mohsensajjaditabar@gmail.com>
2020-09-10 16:57:11 +0430

    reqady to request nahayat

--- LogMg.py
+++ LogMg.py
@@ -14,12 +14,12 @@ fh = RotatingFileHandler('order.log', maxBytes=200000,
 
 fh.setLevel(logging.DEBUG)
 # create console handler with a higher log level
-#ch = logging.StreamHandler()
-#ch.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
-#ch.setFormatter(formatter)
+ch.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(fh)
-#logger.addHandler(ch)
+logger.addHandler(ch)
