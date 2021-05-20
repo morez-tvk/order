eaa445a79e2155479a76fc05c7cbaaadc46d3178
Mohammadreza Tavakolian <tavakolian.mr@gmail.com>
2021-05-20 17:23:06 +0430

    new version

--- nahayat.py
+++ nahayat.py
@@ -1,68 +1,19 @@
 import time
 import pause
 import threading
+import requests
 import datetime
-from requests_futures.sessions import FuturesSession
 from multiprocessing import Process
 import os, signal
 import datetime
 from LogMg import *
+import json
 
+session = requests.session()
 
-delay_list = \
-    [1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-1,
-2,
-2,
-2,
-2,
-2,
-2,
-2,
-2,
-2,
-3,
-3,
-3,
-3,
-5,
-5,
-5,
-10,
-15,
-25,
-25,
-50,
-50,
-50,
-100,
-100,
-100,
-200]
 
 class NahayatNegar:
-    def __init__(self, data, limit_time):
+    def __init__(self, data, limit_time, servers):
         self.data_list = []
         if type(data) == list:
             for sahm in data:
@@ -75,103 +26,39 @@ class NahayatNegar:
             self.data = list(data['data'].keys())[0]
             self.headers = data['headers']
             self.link = data['url']
-        self.time = limit_time
+        self.time = [int(i) for i in limit_time.split(':')]
         self.success = False
         self.time_period = 2
-        self.sem = threading.Semaphore()
+        self.servers = servers
 
-    def multi_req(self, delay=0, time_period=5):
-        print(self.data_list)
-        #self.delay = delay / 1000
-        # final_time = time.mktime(
-        #     datetime.datetime.strptime(self.time, "%Y-%m-%d %H:%M:%S").timetuple()) + time_period
-        #pause.until(datetime.datetime.strptime(self.time, "%Y-%m-%d %H:%M:%S"))
+    def order(self, ot):
+        logger.info('sending first order')
+        data = json.loads(self.data)
+        data['data']['orderTicket'] = ot
         now_time = datetime.datetime.now()
-        logger.info(now_time)
-        #t = Process(target=self.order, daemon=True)
-        logger.info ("t created")
-        pause_until = now_time.replace(hour=20, minute=delay,second=50,microsecond=985000)
-        pause.until(pause_until)
-        self.order()
-        #t.start()
-        #wakeup_time = 2000
-        #time.sleep(wakeup_time)
-        #t.terminate()
-
-    def order(self):
-        logger.info('single ordering')
-        with FuturesSession(max_workers=1) as session:
-            delay_index = 0
-            while delay_index < len(delay_list):
-                logger.info(datetime.datetime.now())
-                future = session.post(url=self.link, cookies=self.cookies, headers=self.headers, data=self.data,
-                                      hooks={'response': self.response_hook}, timeout=1200000)
-                # if delay_index >= len(delay_list):
-                #     logger.info ("failed")
-                #     break
-                logger.info(delay_list [delay_index])
-                time.sleep(delay_list [delay_index]/1000)
-                delay_index += 1
-
-    def sequence_order(self):
-        print('multi item ordering')
-        with FuturesSession(max_workers=1) as session:
-            while True:
-                for sahm in self.data_list:
-                    future = session.post(url=self.link, cookies=self.cookies, headers=self.headers, data=sahm,
-                                          hooks={'response': self.response_hook}, timeout=1200000)
-
-    def response_hook(self, resp, *args, **kwargs):
+        pause_until = now_time.replace(hour=self.time[0], minute=self.time[1], second=self.time[2],
+                                       microsecond=self.time[3])
+        # pause.until(pause_until)
         try:
-            result = resp.json()
-            logger.info(str(result))
-            # if result['done'] == True:
-            #     self.success = True
-            #     logger.info ("done")
+            res = session.post(url=self.link, cookies=self.cookies, headers=self.headers, data=json.dumps(data),
+                               timeout=1200000)
+            ot = res.json()['customer']['orderTicket']
+            data['data']['orderTicket'] = ot
         except Exception as e:
-            logger.info(str(e))
             pass
-
-
-if __name__ == '__main__':
-    data = {
-    "url": "https://www.nahayatnegar.com/online/order/saveOrder",
-    "raw_url": "https://www.nahayatnegar.com/online/order/saveOrder",
-    "method": "post",
-    "cookies": {
-        "_vid": "1598986212782105",
-        "_pk_id.1.97af": "a4caaaea7583eccc.1598986787.8.1599599274.1599451294.",
-        "__auc": "1221212",
-        "_ga": "GA1.2.1463733646.1598986789",
-        "__eid": "e60cb2387b1eea34ef2539baea34820f",
-        "locale_dispatcher": "fa_IR",
-        "ROUTEID": ".1",
-        "__asc": "da2dff221746f8bc2c47d4b8ece",
-        "_pk_ses.1.97af": "1",
-        "_gid": "GA1.2.1330500911.1599599265",
-        "_gat_gtag_UA_56428553_2": "1",
-        "oid": "31860418b51221b560bb1c732a69176b759fe1fdf72f8939",
-        "sid": "f431e17ea0081a3c9e51fc240221ee21fdf72f893924"
-    },
-    "headers": {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0",
-        "Accept": "application/json, text/plain, */*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Content-Type": "application/json;charset=utf-8",
-        "Origin": "https://www.nahayatnegar.com",
-        "Connection": "keep-alive",
-        "Referer": "https://www.nahayatnegar.com/online"
-    },
-    "data": {
-        "{\"data\":{\"split\":false,\"edit\":false,\"price_percent\":[15300,15460,15620,15780,15940,16100,16260,16420,16580,16740,16900],\"minPrice\":15300,\"maxPrice\":16900,\"minLot\":1,\"maxLot\":100000,\"volume_steps\":[10000,20000,30000,40000,50000,60000,70000,80000,90000,100000],\"csrf\":\"ad4e377ce4ae0adba87fb456407a44e5\",\"inst\":\"IRO1TSAN0001\",\"paymentType\":\"2\",\"limitType\":\"1\",\"dueType\":\"1\",\"bondInterest\":null,\"deduct\":{\"cr\":\"0.003040000000\",\"xc\":\"300000000\",\"tx\":\"0.000000000000\",\"bcr\":\"0.000256000000\",\"xbcr\":\"300000000\",\"fcr\":\"0.000000000000\",\"xfcr\":\"99999999999\",\"scr\":\"0.000240000000\",\"xscr\":\"100000000\",\"ccr\":\"0.000080000000\",\"xccr\":\"134000000\",\"tcr\":\"0.000080000000\",\"xtcr\":\"80000000\",\"rbcr\":\"0.000016000000\",\"xrbcr\":\"26000000\"},\"calcShow\":false,\"showCalc\":false,\"grandTotal\":15356792,\"price\":\"15300\",\"volume\":\"1000\",\"draft\":false,\"diffVolume\":\"1000\",\"orderForm\":\"buy\",\"displayVolume\":0}}": ""
-    }
-}
-
-
-
-
-
-    #
-    pause_time = '2020-09-09 01:55:05'
-    a = NahayatNegar(data=data, limit_time=pause_time)
-    print(a.multi_req(delay=300, time_period=100))
+            # logger.error(res.json())
+        logger.info("sending to next server")
+        x = f"http://{self.servers[0]}:2020/get_next"
+        print(x)
+        session.post(x, json={
+            "link": self.link,
+            "cookies": self.cookies,
+            "headers": self.headers,
+            "data": json.dumps(data),
+            "servers": self.servers,
+            "counter": 0
+        })
+        try:
+            logger.info(res.json())
+        except Exception as e:
+            pass
--- run.py
+++ run.py
@@ -1,5 +1,94 @@
-from view import app as application
+from flask import Flask, request, jsonify
+import requests
+from nahayat import NahayatNegar
+import flask
+import time
+from multiprocessing import Process
+import json
+from LogMg import logger
 
-if __name__ == '__main__':
-    # application.run(debug=True)
-    application.run(host='0.0.0.0', port=2020, debug=False)
+# import requests
+app = Flask(__name__)
+app.config["DEBUG"] = True
+import datetime
+
+
+def input_validator(request):
+    try:
+        text_json = request.data
+        json.loads(text_json)
+    except:
+        return False
+    return True
+
+
+oid = 0
+orders = {}
+l_o = {}
+session = requests.Session()
+
+
+@app.route('/nahayat_negar', methods=['post'])
+def first_order():
+    global oid
+    global l_o
+    global orders
+    if not input_validator(request):
+        flask.abort(400, "The input should be json.")
+    text_json = request.data
+    data = json.loads(text_json)
+    if data['status'] == 'set':
+        oid += 1
+        # time_period = data.get('tedad', 20)
+        # delay = data.get('delay', 0)
+        next_ot = data.get("next_ot")
+        orders[oid] = NahayatNegar(data=data['json'], limit_time=data['time'],
+                                   servers=data['servers'])
+        orders[oid].order(next_ot)
+        # t = Process(target=orders[oid].order, args=(next_ot))
+        # t.start()
+        print("wating ...")
+        # orders [oid].multi_req (delay, time_period)
+        # l_o[oid] = t
+        return {'id': oid, 'status': 200}
+
+    elif data['status'] == 'cancel':
+        try:
+            l_o[data['id']].terminate()
+            l_o[data['id']].join(0)
+            orders.pop(data['id'])
+            return {"message": f"order {data['id']} deleted from queue"}
+        except:
+            flask.abort(500, "No such order in queue")
+
+
+@app.route('/get_next', methods=['post'])
+def x():
+    req = request.json
+    logger.info('sending request')
+    try:
+        res = requests.post(req['link'], cookies=req['cookies'], headers=req['headers'], data=req['data'])
+        ot = res.json()['customer']['orderTicket']
+        req['data']['data']['orderTicket'] = ot
+    except KeyError:
+        logger.error(res.json())
+    except Exception as e:
+        logger.error('whattttttttt')
+
+    if req['counter'] + 1 < len(req['servers']):
+        logger.info("sending to next one")
+        session.post(f"{req['servers'][req['counter']]}:2021/get_next", json={
+            "link": req['link'],
+            "cookies": req['cookies'],
+            "headers": req['headers'],
+            "data": json.dumps(req['data']),
+            "servers": req['servers'],
+            "counter": req['counter'] + 1
+        })
+    try:
+        logger.info(res.json())
+    except Exception as e:
+        pass
+
+
+app.run(host='localhost',port=2020)
